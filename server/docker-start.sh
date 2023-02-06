d7a1223da3b55bdff38f1c47702ba0c0a2c04ddd
Tom Waller <tom.waller@versent.com.au>
2023-02-06 15:43:31 +0100

    Increased upload size to 5GB

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -4,5 +4,5 @@ cd /opt/app
 
 alembic upgrade head
 
-echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads"
-exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
+echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads and maximum upload size of ${MAX_UPLOAD_FILE_SIZE_BYTES}"
+exec waitress-serve-3 --port ${PORT} --max-request-body-size 5368709120 szurubooru.facade:app
